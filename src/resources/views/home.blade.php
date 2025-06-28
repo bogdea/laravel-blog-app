04f97278fb5f60e2618b0a6835346ce9b27d5291
Madalin Bogdea <madalinbogdea@icloud.com>
2025-06-28 22:35:43 +0300

    git init

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+
+<body>
+    <h1>hello world</h1>
+
+    @livewireScripts
+</body>
+
+</html>
