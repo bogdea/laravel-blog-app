2e2cc927920116cfd29056183f813b13e2b4cc22
Madalin Bogdea <madalinbogdea@icloud.com>
2025-07-09 23:49:43 +0300

    feat: add blog and home links to header

--- src/resources/views/livewire/navbar.blade.php
+++ src/resources/views/livewire/navbar.blade.php
@@ -1,7 +1,7 @@
 <header class="px-15 flex items-center justify-between pt-10">
-    <h1 class="text-2xl font-semibold">no microtears</h1>
+    <h1 class="text-2xl font-semibold"><a href="/">no microtears</a></h1>
     <nav class="flex space-x-6 font-medium">
-        <a href="#" class="hover:text-[#555]">blog</a>
+        <a href="/blog" class="hover:text-[#555]">blog</a>
         @auth
             <a
                 onclick="Livewire.dispatch('logout')"
